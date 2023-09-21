45699836dfc7aa6e73886b02f1dc2f3698507776
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-09-21 07:35:26 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3)
+console.log("Samar");
+
+let name = "Samar";
+let age = 20;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+// bigint
+// string => ""
+//  boolean => true or false
+// null => standalones value 
+// undefinef
+
+console.log(typeof "Samar");
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
