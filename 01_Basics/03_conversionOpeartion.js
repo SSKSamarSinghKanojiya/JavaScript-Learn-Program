80d9ed3698a10d6b480e5547ad34f61e9ed80818
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-09-21 07:55:38 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOpeartion.js
@@ -0,0 +1,27 @@
+let score = null
+// let score = "33abc"
+/*
+console.log(typeof score);
+console.log(typeof score);  
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+//  33 => 33
+// "33abc " => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; or 0 => false
+// "" => false
+//  "samartha" => true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
