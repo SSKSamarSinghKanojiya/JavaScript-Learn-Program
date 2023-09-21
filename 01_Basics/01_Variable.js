d77f37b7c2ad8a153ade88c953314954c2a3ee60
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-09-21 07:16:53 +0000

    container config done

--- 01_Basics/01_Variable.js
+++ 01_Basics/01_Variable.js
@@ -1,5 +1,16 @@
-const accountNumber = 1234567890;
-let accountEmail = "samar@gmail.com";
-var accountPassword = "54321";
+const accountId = 1234567890;
+let accountEmail = "samar@gmail.com"
+var accountPassword = "54321"
+accountCity = "Delhi"
+let accountState;
 
-console.log(accountEmail);
+// accountId = 2 not allowed
+
+accountEmail = "samar@google.com"
+accountPassword = "12121"
+accountCity = "Riva"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
+
