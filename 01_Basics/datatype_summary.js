eccad3b607d5070eb480e02c8ddb4ed271846d2a
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-09-22 06:33:54 +0000

    datatype summary

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,19 @@
+// Primitives Data types
+//7 : String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// Reference (Non Primitive)
+// Array , Object , Function
+
