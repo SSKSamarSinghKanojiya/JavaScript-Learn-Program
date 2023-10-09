d2e70e2a7167247f4f5989eeb24d03024088f30c
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-10-09 10:27:10 +0000

    array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,25 @@
+    // Array
+
+
+const myArr = [0,1,2,3,4,5,];
+const myHeros = ["Samar", "Singh", "Kanojiya"];
+
+const myArr2 = new Array(1,2,3,4); 
+
+console.log(myArr[1]);
+
+// Array Mathod
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(0);
+// myArr.unshift(9);
+// myArr.shift();
+
+console.log(myArr.includes(9));
+console.log(myArr.includes(1));
+
+
+console.log(myArr);
