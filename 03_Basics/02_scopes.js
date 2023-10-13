6e80b3136f5e4a894d20dac59600208cb3d8ccb7
Samar Singh Kanojiya <69416909+SSKSamarSinghKanojiya@users.noreply.github.com>
2023-10-13 17:54:26 +0000

    Scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -6,11 +6,56 @@ let a = 300;
 if (true) {
   // This is the Block Scope
   let a = 5;
-  console.log("INNER : ", a);
+  // console.log("INNER : ", a);
   b = 6;
   const c = 7;
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Samar"
+
+    function two(){
+        // const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+
+
+if (true) {
+  const username = "Samar Singh"
+  if (username === "Samar Singh") {
+    const website = " Youtube"
+    // console.log(username + website);
+
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+// ----------------------------- Interesting --------------------
+// -------------Mini Hoisting Concept -----------------
+
+console.log(addone(5));
+
+function addone(num){
+  return num + 1;
+}
+
+
+addTwo(5);
+
+const addTwo = function(num){
+  return num + 2;
+}
